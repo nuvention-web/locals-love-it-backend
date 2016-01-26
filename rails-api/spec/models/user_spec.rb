24f6a532272665724dd8c4acf5a9772ff0bb751a
Juan David <juandominguez2017@u.northwestern.edu>
2016-01-25 23:52:01 -0600

    Sets up specs for user model

--- rails-api/spec/models/user_spec.rb
+++ rails-api/spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
-describe User do
-
+describe User, type: :model do
+	it "has a valid factory"
+	it "is invalid without an email"
+	it "is invalid without a password"
+	it "is invalid without a role"
+	it "has an access token after creation"
 end
