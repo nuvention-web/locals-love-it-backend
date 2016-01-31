198bf0a42e581f44f565d602af292a523ee9e4b7
Juan David <juandominguez2017@u.northwestern.edu>
2016-01-31 17:04:15 -0600

    Modifies schema to add influences to db

--- rails-api/db/schema.rb
+++ rails-api/db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127022047) do
+ActiveRecord::Schema.define(version: 20160131213155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "influencers", force: :cascade do |t|
+    t.integer  "user_id",        null: false
+    t.text     "short_bio"
+    t.string   "twitter_handle"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "influencers", ["user_id"], name: "index_influencers_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,9 +40,12 @@ ActiveRecord::Schema.define(version: 20160127022047) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role",                                null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "influencers", "users"
 end
