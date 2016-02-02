41d9800404c5860b6c57a2d15f366318ab20e50a
= <helenfoster2016@u.northwestern.edu>
2016-02-01 20:52:29 -0600

    Adds flat search results, profile, and required bootstrap files

---
+++ react-frontend/js/react-script.js
@@ -0,0 +1,30 @@
+<div class = "col-md-3">
+    <div class = "panel panel-default" id = "panelSearchResult">
+      	<div class = "panel-body">
+     	 	<a href = "profile.html"><img src="images/ReshaProfilePicture.jpg" class="img-responsive img-circle" alt="Resha"></a>
+     	</div>
+     	<div class = "panel-footer" id = "panelSearchResultFooter">
+     		<div class = "influencer-name">
+     			<a href = "profile.html"><h4>Resha</h4></a>
+     		</div>
+     		<div class = "influencer-extra-info">
+     			<ul>
+     				<li> <a href="https://twitter.com/kanYeBreast">42.5k Twitter</a></li>
+     				<li> <a href = "https://www.instagram.com/carnaldish/">2.2k Instagram</a></li>
+     			</ul>
+                <center><span class = "tag">Food</span> <span class = "tag">Cooking</span></center>
+     		</div>
+     	</div>
+     </div>
+</div>
+
+
+
+var ImagePanel = React.createClass({
+    render: function(){
+        return(
+            <div className = "panel-body">
+            </div>
+        );
+    }
+});
