fc2801049553d785433d1eec5d1774e82b147423
Juan David <juandominguez2017@u.northwestern.edu>
2016-02-07 21:15:11 -0600

    Adds influencer controller logic

--- rails-api/app/controllers/influencers_controller.rb
+++ rails-api/app/controllers/influencers_controller.rb
@@ -1,6 +1,11 @@
 class InfluencersController < ApplicationController
 	def index
 		@influencers = Influencer.all
-		render json: @influencers
+		render json: @influencers, each_serializer: InfluencersSerializer, adapter: :json
+	end
+
+	def show
+		@influencer = Influencer.find(params[:id])
+		render json: @influencer
 	end
 end
--- rails-api/spec/controllers/influencers_controller_spec.rb
+++ rails-api/spec/controllers/influencers_controller_spec.rb
@@ -6,31 +6,41 @@ describe InfluencersController, type: :controller do
 		let!(:influencer0) { FactoryGirl.create(:influencer) }
 		let!(:influencer1) { FactoryGirl.create(:influencer_with_industry) }
 
-		it 'returns http success' do
+		before(:each) do
 			get :index
+		end
+
+		it 'returns http success' do
 			expect(response).to be_success
 		end
 
 		it 'returns all the influencers' do
-			get :index
 			expect(assigns[:influencers]).to match_array([influencer0, influencer1])
 		end
 
-		describe 'influencers information' do
-			it 'has a user_id for each influencer' do
-				get :index
-				binding.pry
-				influencers = JSON.parse(response.body)["influencers"]
-				
-				influencers.each do |key, value|
-					puts key
-					puts value
-				end
-			end
-
-
-			it 'has an industry for each influencer'
-
+		it 'follows json api format and uses serializer' do
+			expect{JSON.parse(response.body)["influencers"]}.to_not raise_error
 		end
+
+	end
+
+	describe 'GET /# show' do
+		let!(:influencer0) { FactoryGirl.create(:influencer) }
+		let!(:influencer1) { FactoryGirl.create(:influencer_with_industry) }
+
+		it 'returns http success' do
+			get :show, id: influencer0.id
+			expect(response).to be_success
+		end
+
+		it 'returns a single influencer, given an id' do
+			get :show, id: influencer0.id
+			expect(assigns[:influencer]).to match(influencer0)
+		end
+
+		it 'follows json api format and uses serializer' do
+			expect{JSON.parse(response.body)["influencer"]}.to_not raise_error
+		end
+
 	end
 end
