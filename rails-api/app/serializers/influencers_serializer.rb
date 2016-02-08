31e37d9aa92d266004019a4b098312a9f976d543
Juan David <juandominguez2017@u.northwestern.edu>
2016-02-07 21:03:49 -0600

    Adds influencer serializer spec and serializer

--- rails-api/app/serializers/influencers_serializer.rb
+++ rails-api/app/serializers/influencers_serializer.rb
@@ -1,3 +1,24 @@
 class InfluencersSerializer < ActiveModel::Serializer
+	attributes :user, :social_media_info
+	has_many :industries
+
+
+	def social_media_info
+		{
+			twitter_handle: twitter_handle,
+			twitter_followers: twitter_followers
+
+		}
+	end
+
+	private
+	
+	def twitter_followers
+		1 + rand(1000)
+	end
+
+	def twitter_handle
+		object.twitter_handle
+	end
 
 end
--- rails-api/spec/serializers/influencers_serializer_spec.rb
+++ rails-api/spec/serializers/influencers_serializer_spec.rb
@@ -1,22 +1,47 @@
 require 'spec_helper'
-require 'pry'
 
-xdescribe InfluencersSerializer do
-	let(:resource) { FactoryGirl.build(:influencer_with_industry) }
+describe InfluencersSerializer do
+	let(:resource) { FactoryGirl.create(:influencer_with_industry) }
+	let(:influencer_industry) { resource.industries.first.name }
 
 	let(:serializer) { InfluencersSerializer.new(resource) }
 	let(:serialization) { ActiveModel::Serializer::Adapter.create(serializer) }
+	subject(:influencer) { JSON.parse(serialization.to_json) }
+
 
 	it "has industries" do
-		serialization
+		expect(influencer['industries'].first['name']).to eql(influencer_industry) 	
 	end
 
-	it "has a user_id"
+	describe "user" do
+		let(:user) { influencer['user'] }
 
-	it "has a twitter_handle"
+		it "has a user_id" do
+			expect(user['id']).to eql(resource.user.id)
+		end
 
-	it "has number of follower for twitter"
+		it "has a first name" do
+			expect(user['first_name']).to eql(resource.user.first_name)
+		end
 
-	it "has a first name and last name"
+		it "has a last name" do
+		       expect(user['last_name']).to eql(resource.user.last_name)
+		end	       
+
+	end
+
+	describe "social_media_info" do
+		let(:social_media_info) { influencer['social_media_info'] }
+
+		it "has a twitter_handle" do
+			expect(social_media_info['twitter_handle']).to eql(resource.twitter_handle)
+		end
+
+		it "has number of follower for twitter" do
+			expect(social_media_info['twitter_followers']).to be_an(Integer)
+		end
+	end
+
+
 	
 end
