4b297f4f4a945fd806402cbb995dc87a01a5925c
Juan David <juandominguez2017@u.northwestern.edu>
2016-02-10 18:47:26 -0600

    refactors to only make one api call

--- rails-api/app/serializers/influencers_serializer.rb
+++ rails-api/app/serializers/influencers_serializer.rb
@@ -4,6 +4,7 @@ class InfluencersSerializer < ActiveModel::Serializer
 
 
 	def social_media_info
+		fill_social_media_info
 		{
 			twitter_handle: twitter_handle,
 			twitter_followers: twitter_followers,
@@ -20,12 +21,21 @@ class InfluencersSerializer < ActiveModel::Serializer
 
 	private
 
+	def fill_social_media_info
+		twitter_user = object.twitter_user
+		
+		@instance_options[:twitter_followers] = twitter_user.followers_count
+		@instance_options[:profile_pic] = twitter_user.profile_image_url_https
+	end
+
+
+
 	def profile_pic
-		object.pic_url_from_twitter	
+		@instance_options[:profile_pic]	
 	end
 	
 	def twitter_followers
-		object.num_followers_on_twitter
+		@instance_options[:twitter_followers]
 	end
 
 	def twitter_handle
