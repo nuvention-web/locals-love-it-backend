6dd797a37f8f5d46407ecd9de4b124c8375840db
ClaudeTheCazur <claudesonazurin2017@u.northwestern.edu>
2016-02-21 16:15:19 -0600

    Refactoring the file structure

--- react-frontend/src/routes.jsx
+++ react-frontend/src/routes.jsx
@@ -2,18 +2,15 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-
-
+var browserHistory=require('react-router/lib/browserHistory');
 var Base = require('./components/Base.jsx');
-var Index = require('./components/Index.jsx');
+var Index = require('./Index.jsx');
 var InfluencersResults = require('./components/InfluencersResults.jsx');
 
 var Routes = (
-    <Router>
-        <Route path="/" component={Base} >
-            <Route path="/index" component={Index} />
-            <Route path="/influencersresults" component={InfluencersResults} />
-        </Route>
+    <Router history = {browserHistory}>
+          <Route path="/" component={Index} />
+          <Route path="/influencersresults" component={InfluencersResults} />
     </Router>
 );
 
