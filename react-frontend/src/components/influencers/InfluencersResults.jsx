3d4c032fced7a631e0617f69239242e19c58c396
Juan David <juandominguez2017@u.northwestern.edu>
2016-02-22 15:17:57 -0600

    tries to use Altcontainer

--- react-frontend/src/components/influencers/InfluencersResults.jsx
+++ react-frontend/src/components/influencers/InfluencersResults.jsx
@@ -1,13 +1,40 @@
 var React = require('react');
+var AltContainer = require('alt/AltContainer');
 
 var InfluencersFilters = require('./InfluencersFilters.jsx');
 var InfluencersResultsContent = require('./InfluencersResultsContent.jsx');
 var InfluencersStore = require('../../stores/InfluencersStore.js');
 var InfluencersActions = require('../../actions/InfluencersActions.js');
 
+var InfluencersList = React.createClass({
+	render: function() {
+		return (
+				<ul>
+					{this.props.influencers.map((influencer, i) => {
+						return (
+							<li>{influencer} </li>
+						);
+					})}
+				</ul>
+		);
+	}
+});
+
 var InfluencersResults = React.createClass({
 
+	componentDidMount: function() {
+		InfluencersStore.listen(this.onChange);
+
+		InfluencersActions.getInfluencers();
+	},
+
   render: function() {
+		if (this.props.errorMessage) {
+			return (
+				<div> this.state.errorMessage </div>
+			);
+		}
+
 
     mainContainerStyle = {
       marginTop: 70
@@ -15,6 +42,9 @@ var InfluencersResults = React.createClass({
 
     return (
       <div id = "mainContainer" className = "container" style = {mainContainerStyle}>
+			<AltContainer store={InfluencersStore}>
+				<InfluencersList />
+				</AltContainer>
         <InfluencersFilters />
       <hr/>
         <InfluencersResultsContent />
