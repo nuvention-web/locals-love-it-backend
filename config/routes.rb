a4eee4b40ad9f79a1ebab3d7e962c0f9292fc086
Juan David <juandominguez2017@u.northwestern.edu>
2016-02-25 23:13:00 -0600

    turns api to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   #devise_for :users
 
-  resources :influencers, defaults: {format: :json}, only: [:index, :show]
+  #resources :influencers, defaults: {format: :json}, only: [:index, :show]
+	#match "/influencers/", :controller => 'influencers'
+	Rails.application.routes.draw do
+		  get 'influencers/' => 'influencers#index'
+	end
 
 end
