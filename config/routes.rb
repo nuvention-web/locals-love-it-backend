4da292e2a1462b8bbbbdfc44e28b1126f2f738c5
ClaudeTheCazur <claudesonazurin2017@u.northwestern.edu>
2016-02-28 13:16:37 -0600

    placed root route at influencers search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #devise_for :users
-
+  root 'influencers#search'
 	get '/influencers/search', to: 'influencers#search'
   resources :influencers, only: [:index, :show]
 	
