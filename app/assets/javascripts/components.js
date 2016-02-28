e150731e4d45148fada8a3b36506d893e76d48b0
Juan David <juandominguez2017@u.northwestern.edu>
2016-02-27 20:25:41 -0600

    Adds the correct view of InfluencersResults

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,3 +1,4 @@
 //= require_tree ./components
 
 window.InfluencersResults = require('./components/influencers/InfluencersResults.js.jsx');
+window.InfluencerProfile = require('./components/influencers/InfluencerProfile.js.jsx');
