3b65b184d1b0e79ccc3c5aa55e40f6b2cdbf9d8a
ClaudeTheCazur <claudesonazurin2017@u.northwestern.edu>
2016-02-28 00:55:53 -0600

    updated package.json

---
+++ app/assets/javascripts/components/NavBar.js.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+
+var NavBar = React.createClass({
+  propTypes: {
+  	name:React.propTypes.string
+  },
+
+  render: function(){
+    return (
+      <div className = "navbar navbar-default navbar-fixed-top">
+        <div className = "container-fluid">
+          <div className = "navbar-header">
+            <a className = "navbar-brand" href = "#"> Locals Love It </a>
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+});
+
+module.exports = NavBar;
