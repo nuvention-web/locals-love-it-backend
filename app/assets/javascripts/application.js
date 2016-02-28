ac79546db84b9320c47f0d8753846142a5a081fd
Juan David <juandominguez2017@u.northwestern.edu>
2016-02-28 16:08:08 -0600

    Removes unnecesary line

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,9 @@
 //= require react
 //= require react_ujs
-   ...
+
 // require bootstrap-sprockets
 // require_self
 // require_tree .
+
 // Pull in our top-level components in the main javascript entry point
 require('./components');
