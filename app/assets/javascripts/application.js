a38373098997af3cf5daece5ef2fceda70394545
ClaudeTheCazur <claudesonazurin2017@u.northwestern.edu>
2016-02-28 12:50:35 -0600

    prepared application.js for styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,8 @@
 //= require react
 //= require react_ujs
-//
+   ...
+// require bootstrap-sprockets
+// require_self
+// require_tree .
 // Pull in our top-level components in the main javascript entry point
 require('./components');
