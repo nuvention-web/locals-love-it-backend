53afb0a24808463483fd552a7f7a5f338ca96bb1
Juan David <juandominguez2017@u.northwestern.edu>
2016-02-28 02:35:06 -0600

    Adds InfluencersSearch component

---
+++ app/assets/javascripts/components/influencers/InfluencersSearch.js.jsx
@@ -0,0 +1,43 @@
+var InfluencersSearch = React.createClass({
+	render: function(){
+			var titleStyle = {
+					fontWeight: "bold"
+			};
+
+			var btnStyle = {
+				borderColor: "#ED005F"
+			};
+
+			return(
+				<div className = "container contentContainer"  id = "topContainer">
+				<br />
+				<br />
+				<br />
+
+					<div className ="row" id = "topRow">
+						<div className = "col-md-5"></div>
+						 <div className = "col-md-6">
+							 <h1 style = {titleStyle}> Locals Love It </h1>
+								<p className = "lead" > Connecting small businesses with local level influencers </p>
+							</div>
+					</div>
+
+					<div className ="row">
+						<div className = "col-md-5"></div>
+						<br/>
+						<br/>
+				 </div>
+
+				 <div className = "row">
+					<div className = "col-md-3 col-md-offset-6">
+						<a className="btn btn-default btn-lg btn-block" style = {btnStyle} href="#" role="button" id = "findInfluencers" onClick = {this.props.onSubmit}>Find me influencers!</a>
+					</div>
+				 </div>
+				</div>
+
+			);
+  }
+});
+
+
+module.exports = InfluencersSearch;
