025fb11b4353be5320674d02f8959835a4a38f0a
ClaudeTheCazur <claudesonazurin2017@u.northwestern.edu>
2016-02-28 23:19:39 -0600

    pass in influencers object to react element for rendering

--- app/assets/javascripts/components/influencers/InfluencersResults.js.jsx
+++ app/assets/javascripts/components/influencers/InfluencersResults.js.jsx
@@ -16,7 +16,7 @@ var InfluencersResults = React.createClass({
       <div id = "mainContainer" className = "container" style = {mainContainerStyle}>
 				<InfluencersFilters/>
 				<hr/>
-        <InfluencersResultsContent influencers={this.props.influencers} />
+        <InfluencersResultsContent Influencers={this.props.influencers} />
       </div>
     );
   }
