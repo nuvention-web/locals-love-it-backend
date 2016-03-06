e78797533f808459202a16866a6213b43f12f7d8
Juan David <juandominguez2017@u.northwestern.edu>
2016-03-05 22:57:49 -0600

    Adds influencers_show precompile asset

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 #precompiling all assets
 Rails.application.config.assets.precompile += [/.*\.js/, 'application.css', 'my_app.css.scss']
+Rails.application.config.assets.precompile += %w( influencers/search.css )
