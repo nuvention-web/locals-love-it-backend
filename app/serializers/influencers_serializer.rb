52be8f3e83840d9ec5665c43c91fa157c47965cb
ClaudeTheCazur <claudesonazurin2017@u.northwestern.edu>
2016-03-27 21:01:56 -0500

    adding new fields for filter testing

--- app/serializers/influencers_serializer.rb
+++ app/serializers/influencers_serializer.rb
@@ -1,5 +1,5 @@
 class InfluencersSerializer < ActiveModel::Serializer
-	attributes :user, :social_media_info, :short_bio
+	attributes :user, :social_media_info, :short_bio, :traits
 	has_many :industries
 
 
@@ -19,21 +19,39 @@ class InfluencersSerializer < ActiveModel::Serializer
 		end
 	end
 
+	def traits
+
+		fill_traits_of_user
+		{
+			type_of_promotion: type_of_promotion
+			frequency: frequency
+			personality: personality
+		}
+
+	end
+
+	def traits_for_filters
+		@instance_options[:type_of_promotion] = ["Review", "Photo&Comment","Sale_ff"].sample
+		@isntance_options[:frequency] = ["One", "On-going"].sample
+		@instance_options[:personality] = ["Quirky", "Witty", "Bubbly", "Sassy", "Conservative"].sample
+	end
+
 	private
 
 	def fill_social_media_info
+
 		twitter_user = object.twitter_user
-		
 		@instance_options[:twitter_followers] = twitter_user.followers_count
 		@instance_options[:profile_pic] = twitter_user.profile_image_url_https
+
 	end
 
 
 
 	def profile_pic
-		@instance_options[:profile_pic]	
+		@instance_options[:profile_pic]
 	end
-	
+
 	def twitter_followers
 		@instance_options[:twitter_followers]
 	end
