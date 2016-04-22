8589103ac736e397608d26c5e26534d416f508ff
Juan David <juandominguez2017@u.northwestern.edu>
2016-04-21 21:10:24 -0500

    Adds the rest of the site back

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,3 +1,5 @@
 //= require_tree ./components
 window.NavBar = require('./components/NavBar');
 window.InfluencersSearch = require('./components/influencers/InfluencersSearch');
+window.InfluencersResults = require('./components/influencers/InfluencersResults');
+window.InfluencerProfile = require('./components/influencers/InfluencerProfile');
