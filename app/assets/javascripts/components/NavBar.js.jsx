b0ee7482254826a52ad548e84ff1b9d6f91b63d6
= <helenfoster2016@u.northwestern.edu>
2016-04-24 12:29:31 -0500

    adds working logo to NavBar

--- app/assets/javascripts/components/NavBar.js.jsx
+++ app/assets/javascripts/components/NavBar.js.jsx
@@ -4,7 +4,7 @@ var NavBar = React.createClass({
       <div className = "navbar navbar-default navbar-fixed-top topNavBar">
         <div className = "container-fluid">
           <div className = "navbar-header">
-            <a className = "navbar-brand" href = "#"><img src="<%= asset_url('LLI_logo_final.png') %>" /></a>
+            <a className = "navbar-brand" href = "#"><img src="http://s31.postimg.org/5efn0o6tz/LLI_logo_final_small.png" /></a>
           </div>
         </div>
       </div>
