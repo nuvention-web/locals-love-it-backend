f90e7611ba55baf99c650f05672565b3b2a19071
= <helenfoster2016@u.northwestern.edu>
2016-04-24 13:30:50 -0500

    shorter sign in link

--- app/assets/javascripts/components/NavBar.js.jsx
+++ app/assets/javascripts/components/NavBar.js.jsx
@@ -2,9 +2,12 @@ var NavBar = React.createClass({
   render: function(){
     return (
       <div className = "navbar navbar-default navbar-fixed-top topNavBar">
-        <div className = "container-fluid">
+        <div className = "container">
           <div className = "navbar-header">
-            <a className = "navbar-brand" href = "#"><img src="http://s31.postimg.org/5efn0o6tz/LLI_logo_final_small.png" /></a>
+            <a className = "navbar-brand" href = "#"><img src="http://s31.postimg.org/8zhj7as2f/LLI_logo_final_small.png" /></a>
+          </div>
+          <div className = "nav navbar-nav navbar-right">
+            <button type="submit" className="btn btn-default" href = "#">Login</button>
           </div>
         </div>
       </div>
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:sessions]
+  as :user do
+	get 'signin' => 'devise/sessions#new', :as => :new_user_session
+	post 'signin' => 'devise/sessions#create', :as => :user_session
+  end
   root 'influencers#search'
 	get '/influencers/search', to: 'influencers#search'
   resources :influencers, only: [:index, :show]
