a729a0e11e84adb742c24f08c67e283bf0c7dfa9
= <helenfoster2016@u.northwestern.edu>
2016-04-26 15:55:57 -0500

    NavBar shows login or logout depending on user status

--- app/assets/javascripts/components/NavBar.js.jsx
+++ app/assets/javascripts/components/NavBar.js.jsx
@@ -1,13 +1,18 @@
 var NavBar = React.createClass({
+
+  getInitialState: function() { 
+     return this.props.link == "/signin" ? {text:"Login"} : {text: "Logout"};
+  },
+
   render: function(){
     return (
       <div className = "navbar navbar-default navbar-fixed-top topNavBar">
         <div className = "container">
           <div className = "navbar-header">
-            <a className = "navbar-brand" href = "/"><img src="http://s31.postimg.org/8zhj7as2f/LLI_logo_final_small.png" /></a>
+            <a className = "navbar-brand" href = {this.props.link}><img src="http://s31.postimg.org/8zhj7as2f/LLI_logo_final_small.png" /></a>
           </div>
           <div className = "nav navbar-nav navbar-right">
-            <a href = "/signin"><button type="submit" className="btn btn-default">Login</button></a>
+            <a href = "/signin"><button type="submit" className="btn btn-default">{this.state.text}</button></a>
           </div>
         </div>
       </div>
