5cef0cf02f611584f81bd470ab9af5659c3b2314
Juan David <juandominguez2017@u.northwestern.edu>
2016-04-25 21:28:14 -0500

    sets business owner as default role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	before_validation :set_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +7,8 @@ class User < ActiveRecord::Base
   has_one :influencer, :dependent => :destroy
   enum role: [:admin, :influencer, :business_owner]
   validates :role, presence: true
+
+	def set_role()
+		self.role ||= :business_owner
+	end
 end
