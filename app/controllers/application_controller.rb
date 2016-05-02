76811b65d202f6ac33d47541adc5d26be9122502
Juan David <juandominguez2017@u.northwestern.edu>
2016-05-01 21:47:18 -0500

    Adds conversation helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
-    helper_method :mailbox
+    helper_method :mailbox, :conversation
 
     def mailbox
 	@mailbox ||= current_user.mailbox
     end
+
+		def conversation
+			@conversation ||= mailbox.conversations.find(params[:id])
+		end
 
 
 
