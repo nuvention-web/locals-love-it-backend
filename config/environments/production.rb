0a618d106b028e9227d159f28ccc492b154cf060
Juan David <juandominguez2017@u.northwestern.edu>
2016-05-15 17:29:05 -0500

    Adds actionmailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,8 @@ class NoCompression
   end
 end
 
+ActionMailer::Base.perform_deliveries = true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
