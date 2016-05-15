0092c4552eafc372084ef5e9b844fb292640b5fa
Juan David <juandominguez2017@u.northwestern.edu>
2016-05-15 18:04:08 -0500

    Uses new registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root 'influencers#search'
 	get '/influencers/search', to: 'influencers#search'
   resources :influencers, only: [:index, :show]
