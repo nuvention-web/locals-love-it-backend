82b33af66627973765595bd5c6016a13e23bdce1
Juan David <juandominguez2017@u.northwestern.edu>
2016-05-16 14:40:21 -0500

    Adds a password generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,22 @@
 require 'csv'
 
+def makePassword(f_name,l_name, u_id)
+	name_part = ''
+	p_word = ''
+	puts f_name
+	puts l_name
+	puts u_id
+	if not l_name.empty?
+		name_part = f_name[0..2] + l_name[0..2]
+		p_word = name_part + (f_name.length + l_name.length).to_s + u_id.to_s
+	else
+		name_part = f_name[0..5]
+		p_word = name_part + (f_name.length).to_s + u_id.to_s
+	end
+
+	p_word
+end
+
 def seedInfluencers
 	fashion_industry = FactoryGirl.build(:industry, name: 'Fashion') 
 	fashion_industry.save! unless Industry.exists?(name: fashion_industry.name)
@@ -10,12 +27,16 @@ def seedInfluencers
 	influencers = CSV.parse(influencers_text, headers: true);
 
 	influencers.each do |influencer|
+		c = 1
 		unless influencer["Twitter Handle #1"].nil?
 			u_name = influencer["Influencer Name"]	
 			f_name = u_name.partition(" ").first
 			l_name = u_name.partition(" ").last
-			u = FactoryGirl.build(:user, role: :influencer, first_name: f_name, last_name: l_name) 
+			pword = makePassword(f_name, l_name, c)
+			u = FactoryGirl.build(:user, role: :influencer, first_name: f_name, last_name: l_name, password: pword) 
+			puts pword
 			u.save! unless User.exists?(u)
+			puts u.inspect
 
 			bio = influencer["Bio"]
 			t_h_with_symbol = influencer["Twitter Handle #1"].tr(' ', '')
@@ -27,6 +48,7 @@ def seedInfluencers
 			i.industries << fashion_industry
 
 			puts i.inspect
+			c = c + 1
 		end
 
 	end
