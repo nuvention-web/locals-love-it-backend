120d9b7dab0a45ac2bf2fd8f4843447b7642df18
Juan David <juandominguez2017@u.northwestern.edu>
2016-05-24 01:07:25 -0500

    Adds user addition to logic

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,22 @@
 class RegistrationsController < Devise::RegistrationsController
+	def create_without_response
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+    end
+	end
 
 	def new_influencer
 		@influencer = Influencer.new
@@ -7,8 +25,8 @@ class RegistrationsController < Devise::RegistrationsController
 	end
 
 	def create_influencer
-		create
-		redirect_to 'influencers/join'
+		create_without_response
+		redirect_to '/influencers/join'
 	end
 
 	private
@@ -20,4 +38,10 @@ class RegistrationsController < Devise::RegistrationsController
 			params[:influencer][:user].permit(:first_name, :last_name, :email, :password, :password_confirmation).merge({role: 'influencer'})
 		end
 	end
+
+  def set_flash_message!(key, kind, options = {})
+    if is_flashing_format?
+      set_flash_message(key, kind, options)
+    end
+  end
 end
