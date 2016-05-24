2fb5d39c4d8f049009cb3ad9a2d28aa8cf59df86
= <helenfoster2016@u.northwestern.edu>
2016-05-23 21:44:23 -0500

    views by country can come from link.  Real data commented out, dummy data showing

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -19,6 +19,8 @@ class UrlsController < ApplicationController
       @clicks = @url.user_clicks
       @by_day = @url.clicks_by_day
 
+
+      #### Clicks per Day Graph ####
       click_array = Hash.new
       click_data = Hash.new
 
@@ -27,21 +29,44 @@ class UrlsController < ApplicationController
         click_data[element.day_start] = element.clicks
       end
 
-       @click_array = click_array
-       @click_data = click_data
 
-      #From Social Shares
+      #Test Data
+       @click_data = click_array
+
+       #Real Data
+       #@click_data = click_data
+
+      #### Social Shares Graph ####
+
+      # Real Data
       #@fb_shares = SocialShares.facebook(u)
       #@pinterest_shares = SocialShares.pinterest(u)
       #@reddit_shares = SocialShares.reddit(u)
       #@linkedin_shares = SocialShares.linkedin(u)
       #@google_shares = SocialShares.google(u)
 
+      #Test Data
       @fb_shares = 12
       @pinterest_shares = 5
       @reddit_shares = 3
       @linkedin_shares = 0
       @google_shares = 8
+
+      #### Countries Graph ####
+      country_hash = Hash.new
+      @countries = @url.countries
+      @url.countries.each do |element|
+        country_hash[element.country] = element.clicks
+        puts element.country
+        puts element.clicks
+      end
+
+      #Real Data
+      #@countries = country_hash
+
+      #Test Data
+      @countries = {"US" => 12, "China" =>5, "Canada" => 8, "France" => 10}
+
 
 
   end
