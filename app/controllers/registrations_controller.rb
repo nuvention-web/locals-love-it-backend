a3f48e00c95f0b4afa3491719ca02a99ced4769c
Juan David <juandominguez2017@u.northwestern.edu>
2016-05-24 01:19:53 -0500

    Adds better redirect logic

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,22 +1,4 @@
 class RegistrationsController < Devise::RegistrationsController
-	def create_without_response
-    build_resource(sign_up_params)
-
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
-      if resource.active_for_authentication?
-        set_flash_message! :notice, :signed_up
-        sign_up(resource_name, resource)
-      else
-        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-        expire_data_after_sign_in!
-      end
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-    end
-	end
 
 	def new_influencer
 		@influencer = Influencer.new
@@ -25,8 +7,25 @@ class RegistrationsController < Devise::RegistrationsController
 	end
 
 	def create_influencer
-		create_without_response
-		redirect_to '/influencers/join'
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+				redirect_to 'influencers/'
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+				redirect_to '/'
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+			redirect_to action: 'new_influencer', twitter_handle: params[:twitter_handle]
+    end
 	end
 
 	private
@@ -37,7 +36,7 @@ class RegistrationsController < Devise::RegistrationsController
 		else
 			params[:influencer][:user].permit(:first_name, :last_name, :email, :password, :password_confirmation).merge({role: 'influencer'})
 		end
-	end
+	end	
 
   def set_flash_message!(key, kind, options = {})
     if is_flashing_format?
