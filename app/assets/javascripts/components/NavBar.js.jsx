d6625c20d57f3e218ebd8be9eaadeef2329f4911
= <helenfoster2016@u.northwestern.edu>
2016-05-29 17:42:30 -0500

    styling fixes to navbar links

--- app/assets/javascripts/components/NavBar.js.jsx
+++ app/assets/javascripts/components/NavBar.js.jsx
@@ -2,10 +2,11 @@ var NavBar = React.createClass({
 
 
   render: function(){
-	var signed_in = this.props.link == '/users/sign_in';
+	  var signed_in = this.props.link == '/users/sign_in';
     var text = signed_in ? 'Sign in' : 'Sign Out';
     var text2 = 'Inbox'
-	this.props.inbox = '/mailbox/inbox'
+	  this.props.inbox = '/mailbox/inbox'
+
     return (
       <div className = "navbar navbar-default navbar-fixed-top" id = "nav">
         <div className = "container-fluid">
@@ -13,21 +14,21 @@ var NavBar = React.createClass({
             <a className = "navbar-brand" href = "/"><img src="http://s32.postimg.org/5ype92u4x/imageedit_2_3348609615.gif" /></a>
           </div>
           <div className = "nav navbar-nav navbar-right">
-		  <a href ={this.props.inbox}>
-		 {(() => {
-        switch (signed_in) {
-          case false:   return <button type="button" className="btn btn-primary navbar-btn">{text2}</button>
-          default:
-        }
-      	})()}
-		  </a>
-      <a href ='/urls/new'><button type="button" className="btn btn-primary navbar-btn">Campaigns</button>
-		  </a>
+            <ul className="nav navbar-nav">
+		          <li> <a href ={this.props.inbox}>
+		            {(() => {
+                  switch (signed_in) {
+                    case false:   return <button type="button" className="btn btn-primary navbar-btn">{text2}</button>
+                    default:
+                  }
+      	        })()}
+		          </a></li>
+              <li>
+                <a href ='/urls/new'><button type="button" className="btn btn-primary navbar-btn">Campaigns</button>
+		          </a></li>
+                          <li><a href = {this.props.link}><button type="button" className="btn btn-primary navbar-btn">{text}</button></a></li>
+            </ul>
 
-
-
-
-            <a href = {this.props.link}><button type="button" className="btn btn-primary navbar-btn">{text}</button></a>
           </div>
         </div>
       </div>
