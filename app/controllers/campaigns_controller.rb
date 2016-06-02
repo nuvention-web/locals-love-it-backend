5327f35f583216d8f3d1a3fb37b2cbe0e8e29548
= <helenfoster2016@u.northwestern.edu>
2016-06-02 00:33:38 -0500

    Adds show action for campaign, links to campaign show from list

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -6,4 +6,11 @@ class CampaignsController < ApplicationController
   def index
     @campaigns = Campaign.where("user_id = ? OR influencer_id = ?", current_user.id, current_user.id)
   end
+
+  def show
+    @campaign = Campaign.find(params[:id])
+    #@i = Influencer.find(@campaign.influencer_id)
+    @i_name = "Donald the Duck"
+    @smb = User.find(@campaign.user_id)
+  end
 end
