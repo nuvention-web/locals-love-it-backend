2510d41476681f417074b52dbc1d7ad18d3bfed2
= <helenfoster2016@u.northwestern.edu>
2016-06-01 23:26:20 -0500

    Passing in campaigns relative to users to /campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,9 @@
 class CampaignsController < ApplicationController
+
+  # GET /campaigns
+  # campaigns/index.html.erb
+  # displays all campaigns relevant to the user
+  def index
+    @campaigns = Campaign.where("user_id = ? OR influencer_id = ?", current_user.id, current_user.id)
+  end
 end
