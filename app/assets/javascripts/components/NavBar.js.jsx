4f82a3627b99e6dbf2f93ff04706b56f4e12d73e
= <helenfoster2016@u.northwestern.edu>
2016-06-02 00:00:10 -0500

    Changes campaigns link in navbar to /campaigns from /urls/new

--- app/assets/javascripts/components/NavBar.js.jsx
+++ app/assets/javascripts/components/NavBar.js.jsx
@@ -24,7 +24,7 @@ var NavBar = React.createClass({
 		          </a></li>
 
 
-              <li> <a href = 'urls/new'>
+              <li> <a href = '/campaigns'>
                 {(() => {
                   switch (signed_in) {
                     case false:   return <button type="button" className="btn btn-primary navbar-btn">Campaigns</button>
