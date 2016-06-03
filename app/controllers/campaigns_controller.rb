163d8a7b984265a3d741c1cf542e3c6c06a09b2c
= <helenfoster2016@u.northwestern.edu>
2016-06-03 14:18:04 -0500

    adds basic stats to campaign show

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -12,16 +12,15 @@ class CampaignsController < ApplicationController
   def show
     @campaign = Campaign.find(params[:id])
     #@i = Influencer.find(@campaign.influencer_id)
-    @i_name = "Donald the Duck"
+    @i_name = "Influencer Name"
     @smb = User.find(@campaign.user_id)
 
     ##Bitly Jawn
     client = Bitly.client
     @link = client.expand(@campaign.link)
-    clicks_by_day = @link.clicks_by_day()
-    puts "CLICKS PER DAY: "
-    puts clicks_by_day
-    puts "HERE IS TEST STUFF"
+    @clicks = @link.user_clicks()
+    @long_url = @link.long_url()
+
     #puts @link.countries()
     #puts @link.global_clicks()#this is the total count of clicks to all Bitlinks that point to the same same long url
     #puts @link.user_clicks() #this is the total count of clicks to this user's Bitlink
@@ -30,7 +29,7 @@ class CampaignsController < ApplicationController
     click_array = Hash.new
     click_data = Hash.new
 
-    clicks_by_day.each do |element|
+    @link.clicks_by_day().each do |element|
       click_array[element.day_start] = rand(0...5)
       puts "HERE"
       puts click_array[element.day_start]
