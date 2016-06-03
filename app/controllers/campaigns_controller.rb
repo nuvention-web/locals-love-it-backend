864ad411cda02873b275a86dd03ed197659573b5
= <helenfoster2016@u.northwestern.edu>
2016-06-03 14:13:22 -0500

    adds views per day graph to campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,3 +1,5 @@
+require 'gchart'
+require 'social_shares'
 class CampaignsController < ApplicationController
 
   # GET /campaigns
@@ -12,5 +14,32 @@ class CampaignsController < ApplicationController
     #@i = Influencer.find(@campaign.influencer_id)
     @i_name = "Donald the Duck"
     @smb = User.find(@campaign.user_id)
+
+    ##Bitly Jawn
+    client = Bitly.client
+    @link = client.expand(@campaign.link)
+    clicks_by_day = @link.clicks_by_day()
+    puts "CLICKS PER DAY: "
+    puts clicks_by_day
+    puts "HERE IS TEST STUFF"
+    #puts @link.countries()
+    #puts @link.global_clicks()#this is the total count of clicks to all Bitlinks that point to the same same long url
+    #puts @link.user_clicks() #this is the total count of clicks to this user's Bitlink
+
+    #### Clicks per Day Graph ####
+    click_array = Hash.new
+    click_data = Hash.new
+
+    clicks_by_day.each do |element|
+      click_array[element.day_start] = rand(0...5)
+      puts "HERE"
+      puts click_array[element.day_start]
+      click_data[element.day_start] = element.clicks
+    end
+
+
+    #Test Data
+    puts @click_data
+     @click_data = click_array
   end
 end
